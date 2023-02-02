608203d8780111b5d22b39c15418275421dbb953
zuikeaidetimo <zggzggg@naver.com>
2023-02-03 02:35:15 +0900

    feat: Compare guess and answer, Print the Result

--- numguess.py
+++ numguess.py
@@ -7,7 +7,13 @@ print(answer)
 
 # Get user's name, guess
 user_name = input('Hello, there! What is your name? ')
-guess = input(f'Hi, {user_name}. Guess the number(1-100): ')
+guess = int(input(f'Hi, {user_name}. Guess the number(1-100): '))
 
 # print to check
 print(user_name, guess)
+
+# Check add print correct or not
+if guess==answer:
+    print('Congrats!')
+else:
+    print(f'Your are wrong! The answer was {answer}.')
